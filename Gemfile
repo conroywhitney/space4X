26ce2debad5520308badf1a581df430418f83f66
Conroy <conroy@quirky.com>
2012-11-04 10:43:30 -0500

    initial rails generation

---
+++ Gemfile
@@ -0,0 +1,24 @@
+#ruby=1.9.3
+#ruby-gemset=space4x
+
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+
+group :development do
+  gem 'debugger'
+  gem 'unicorn'
+end
+
+group :test do
+
+end
+
+group :staging do
+
+end
+
+group :production do
+
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Space4X::Application.config.secret_token = 'a666dc76918867126fbb10a6dd95a1cced37f76a2a1146374dd264c0b30cece3d23bca6bd027a0b62687cf283f457d2123f839a92b21aaff28e9425ea043cda7'
